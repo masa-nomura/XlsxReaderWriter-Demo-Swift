6e7de9c40e5450b38a581a3c607e0d768c8d161e
Masakazu Nomura <hb63598fd7ur5y@softbank.ne.jp>
2018-06-20 08:57:36 +0900

    Initial Commit

---
+++ iWakkaRecords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iWakkaRecords
+//
+//  Created by Masakazu on 2018/06/20.
+//  Copyright © 2018 Masakazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iWakkaRecordsTests/iWakkaRecordsTests.swift
@@ -0,0 +1,36 @@
+//
+//  iWakkaRecordsTests.swift
+//  iWakkaRecordsTests
+//
+//  Created by Masakazu on 2018/06/20.
+//  Copyright © 2018 Masakazu. All rights reserved.
+//
+
+import XCTest
+@testable import iWakkaRecords
+
+class iWakkaRecordsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
